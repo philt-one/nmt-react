fc485813c82f0c053c52d0c1218549e4c8bf3001
Philippe Talarico <p.talarico20@gmail.com>
2020-03-19 12:24:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ yarn install
 cd ./api && pip install -r requirements.txt
 mkdir model && cd model 
 wget https://s3.amazonaws.com/opennmt-models/averaged-ende-export500k-v2.tar.gz
-unzip averaged-ende-export500k-v2.tar.gz
-rm averaged-ende-export500k-v2.tar.gz
+tar -xzvf averaged-ende-export500k-v2.tar.gz && rm averaged-ende-export500k-v2.tar.gz
 
 
